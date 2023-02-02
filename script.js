38de6a497410060b6c2ae57a5dab3e9ac47ad905
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-02-02 17:23:24 +0200

    first version

---
+++ script.js
@@ -0,0 +1,27 @@
+function calc() {
+    let operand = prompt('Please, enter an operand');
+    let firstNum = Number(prompt('Please, enter first number'));
+    let secondNum = Number(prompt('Please, enter second number'));
+    return alert(`Your result is: ${calculation(firstNum, secondNum, operand)}`);
+}
+
+function calculation(firstNum, secondNum, operand) {
+    let result;
+    switch (operand) {
+        case '+':
+            result = firstNum + secondNum;
+            break;
+        case '-':
+            result = firstNum - secondNum;
+            break;
+        case '*':
+            result = firstNum * secondNum;
+            break;
+        case '/':
+            result = firstNum / secondNum;
+            break;
+    }
+    return result;
+}
+
+calc();
